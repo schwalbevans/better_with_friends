58da6c1bcc197ba22bb68a7ade58075c146144ad
Evan <evanschwalb@gmail.com>
2022-08-15 16:51:17 -0700

    Authentication method updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:better_with_friends/screens/welcome_screen.dart';
 import 'package:better_with_friends/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
   runApp(MyApp());
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:better_with_friends/constants.dart';
 import 'package:better_with_friends/screens/components/background.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 
 
@@ -77,7 +78,21 @@ class SecondRoute extends StatelessWidget {
           ),
           SizedBox(height: 30),
           ElevatedButton(
-          onPressed: () {
+          onPressed: () async {
+            try {
+              UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: "barry.allen@example.com",
+                  password: "SuperSecretPassword!"
+              );
+            } on FirebaseAuthException catch (e) {
+              if (e.code == 'weak-password') {
+                print('The password provided is too weak.');
+              } else if (e.code == 'email-already-in-use') {
+                print('The account already exists for that email.');
+              }
+            } catch (e) {
+              print(e);
+            }
             Navigator.pop(context);
           },
           child: const Text('Signup'),
