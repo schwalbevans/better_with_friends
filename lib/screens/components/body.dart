69a721708efdc3c2bb13232c7260ef67fd20d7b0
Evan <evanschwalb@gmail.com>
2022-08-18 21:00:57 -0700

    SLight Change, Login now connected to signup page for testing

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -25,7 +25,7 @@ class Body extends StatelessWidget {
             ),
             onPressed: (){Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Signup()),
+                MaterialPageRoute(builder: (context) => Login()),
                 );
               },
             child: Text("LOGIN")),
@@ -110,6 +110,72 @@ class Signup extends StatelessWidget {
         ),
     ]
       ),
+      ),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  Login({Key? key}) : super(key: key);
+  final myController = TextEditingController();
+  final passController = TextEditingController();
+
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    passController.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Signup'),
+      ),
+      body:
+      Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: myController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your username',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your password',
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                        email: myController.text,
+                        password: passController.text
+                    );
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'weak-password') {
+                      print('The password provided is too weak.');
+                    } else if (e.code == 'email-already-in-use') {
+                      print('The account already exists for that email.');
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+                  Navigator.pop(context);
+                },
+                child: const Text('Signup'),
+              ),
+            ]
+        ),
       ),
     );
   }
