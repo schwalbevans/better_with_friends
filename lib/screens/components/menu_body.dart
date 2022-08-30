a59f22ebd9f0f216c7c31de2f5a43039642129c2
Evan <evanschwalb@gmail.com>
2022-08-29 17:18:12 -0700

    Added Button to login screen

--- lib/screens/components/menu_body.dart
+++ lib/screens/components/menu_body.dart
@@ -1,17 +1,24 @@
 
+import 'dart:ffi';
 import 'package:better_with_friends/screens/components/background_menu.dart';
 import 'package:flutter/material.dart';
 
 class menuBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Background_Menu(child: Column(
+    return Background_Menu(
+      child: Column(
         //mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[Text("List of Habits",
+        children: const <Widget>[
+          ElevatedButton(onPressed: null,
+              child: Text('Signup')),
+          Text("List of Habits",
         style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30)
-
     ),
+
+
     ],
+
     ),
     );
   }
