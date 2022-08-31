d24e1030b0d9d0f9d8b61f79314d6d12d1e8905d
Evan <evanschwalb@gmail.com>
2022-08-30 17:05:38 -0700

    Added dialog box to add habit

--- lib/screens/components/menu_body.dart
+++ lib/screens/components/menu_body.dart
@@ -1,25 +1,45 @@
 
-import 'dart:ffi';
+
+
+
 import 'package:better_with_friends/screens/components/background_menu.dart';
 import 'package:flutter/material.dart';
 
 class menuBody extends StatelessWidget {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Background_Menu(
       child: Column(
         //mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          ElevatedButton(onPressed: null,
-              child: Text('Signup')),
-          Text("List of Habits",
+        children: <Widget>[
+          ElevatedButton(
+              onPressed: () {
+                openDialog(context);
+              },
+              child: const Text('Add a Habit')),
+          const Text("List of Habits",
         style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30)
     ),
-
 
     ],
 
     ),
+
     );
+
   }
+
+void openDialog(context) => showDialog( builder: (context) => AlertDialog(
+    title: Text('New Habit'),
+    content: TextField(
+      decoration: InputDecoration(hintText: 'Enter your Habit Name'),
+    ),
+    actions: [
+      TextButton(onPressed: () {}, child: Text('SUBMIT')),
+    ],
+  ), context:context,
+  );
 }
